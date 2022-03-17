20ba8b5877e0ad1df9555a6b42f077dc265940c9
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-03-17 21:38:17 +0200

    Fixed details pane

--- server/pokemon.go
+++ server/pokemon.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -8,14 +9,20 @@ import (
 )
 
 var PokeStyle = lipgloss.NewStyle().
-	Width(68).Height(32).
+	Width(68).Height(28).
 	BorderStyle(lipgloss.NormalBorder()).
 	BorderForeground(lipgloss.Color("63"))
+var DetailStyle = lipgloss.NewStyle().Bold(true).Width(36).Height(4)
+var Intructions = DetailStyle.Width(24).Render(`
+Use ^v to browse%s
+Use <> to change form%s
+Use space to select%s`)
 
 type PokeModel struct {
 	Content string //68 x 28(56)
 	Index   string
 	Form    int
+	Details string
 }
 
 func (m *PokeModel) Init() tea.Cmd { return nil }
@@ -30,12 +37,20 @@ func (m *PokeModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 func (m *PokeModel) View() string {
-	return PokeStyle.Render(m.Content)
+	return PokeStyle.Render(m.Content + m.Details)
 }
 
 func InitialPoke() *PokeModel {
 	logo, _ := os.ReadFile("logo.cow")
-	return &PokeModel{string(logo), "001", 0}
+	intro := fmt.Sprintf(Intructions, "           Pokedex Over SSH!",
+		"      Made with charmbracelet/charm toolkit", "        Source at github.com/SHA65536/Charmeleon")
+	return &PokeModel{string(logo), "001", 0, intro}
+}
+
+func (m *PokeModel) UpdateDetails() {
+	newdetails := fmt.Sprintf(Intructions, "              Name: "+Pokedex[m.Index].Name,
+		"         Index: "+m.Index, "           Form: "+Pokedex[m.Index].Forms[m.Form].Name)
+	m.Details = newdetails
 }
 
 func (m *PokeModel) UpdatePokemon(idx string) {
@@ -43,6 +58,7 @@ func (m *PokeModel) UpdatePokemon(idx string) {
 	m.Form = 0
 	dat, _ := os.ReadFile(Pokedex[idx].Forms[0].Cow)
 	m.Content = string(dat)
+	m.UpdateDetails()
 }
 
 func (m *PokeModel) UpdateForm(direction int) {
@@ -53,4 +69,5 @@ func (m *PokeModel) UpdateForm(direction int) {
 	}
 	dat, _ := os.ReadFile(Pokedex[m.Index].Forms[m.Form].Cow)
 	m.Content = string(dat)
+	m.UpdateDetails()
 }
